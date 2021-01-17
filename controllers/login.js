3dcb46104025798ed182781553be1d234e9542e0
ai-small <62231699+ai-small@users.noreply.github.com>
2021-01-17 21:35:17 +1200

    commented res.cookie secure parameter for development purposes

--- controllers/login.js
+++ controllers/login.js
@@ -19,7 +19,7 @@ const login = (req, res, next) => {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
           sameSite: 'none',
-          secure: true,
+          //secure: true,
         })
         .status(200)
         .send({ message: authIsSuccessMessage })
